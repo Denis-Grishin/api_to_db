7f32594df71f72485d8b573807b8ece30e9b85e0
Denis-Grishin <denis.y.grishin@gmail.com>
2023-07-26 14:41:39 +1000

    updated predictions data

--- app/routers/prediction.py
+++ app/routers/prediction.py
@@ -5,7 +5,7 @@ import httpx
 import schedule
 import time
 from sqlalchemy.orm import Session
-from sqlalchemy import exists, insert 
+from sqlalchemy import exists, insert, update, delete
 from .. import models, schemas, utils
 from ..database import get_db
 from ..config import settings
@@ -32,43 +32,84 @@ async def create_prediction(fixture_id: int, db: Session = Depends(get_db)):
     data = response.json()
     prediction = data['response'][0]
 
+    print(f"Retrieved prediction data for fixture_id: {fixture_id}")
+
     if db.query(exists().where(models.Predictions.fixture_id == fixture_id)).scalar():
-        # Skip if prediction already exists in the database
-        print(f"Prediction with fixture_id {fixture_id} already exists.")
-        return {"message": f"Prediction with fixture_id {fixture_id} already exists."}
+        # Update prediction if it already exists in the database
+        print(f"Prediction with fixture_id {fixture_id} already exists. Updating...")
 
-    home_team_name = prediction['teams']['home']['name']
-    away_team_name = prediction['teams']['away']['name']
+        home_team_name = prediction['teams']['home']['name']
+        away_team_name = prediction['teams']['away']['name']
 
-    pred_row = {
-        'fixture_id': fixture_id,
-        'predictions_winner_id': prediction['predictions']['winner']['id'],
-        'predictions_winner_name': prediction['predictions']['winner']['name'],
-        'predictions_winner_comment': prediction['predictions']['winner']['comment'],
-        'predictions_win_or_draw': prediction['predictions']['win_or_draw'],
-        'predictions_under_over': prediction['predictions']['under_over'],
-        'predictions_goals_home': prediction['predictions']['goals']['home'],
-        'predictions_goals_away': prediction['predictions']['goals']['away'],
-        'predictions_advice': prediction['predictions']['advice'],
-        'predictions_percent_home': prediction['predictions']['percent']['home'],
-        'predictions_percent_draw': prediction['predictions']['percent']['draw'],
-        'predictions_percent_away': prediction['predictions']['percent']['away'],
-        'league_id': prediction['league']['id'],
-        'league_name': prediction['league']['name'],
-        'league_country': prediction['league']['country'],
-        'league_logo': prediction['league']['logo'],
-        'league_flag': prediction['league']['flag'],
-        'league_season': prediction['league']['season'],
-        'teams_home_id': prediction['teams']['home']['id'],
-        'teams_home_name': prediction['teams']['home']['name'],
-        'teams_home_logo': prediction['teams']['home']['logo'],
-        'teams_away_id': prediction['teams']['away']['id'],
-        'teams_away_name': prediction['teams']['away']['name'],
-        'teams_away_logo': prediction['teams']['away']['logo']
-    }
+        pred_row = {
+            'fixture_id': fixture_id,
+            'predictions_winner_id': prediction['predictions']['winner']['id'],
+            'predictions_winner_name': prediction['predictions']['winner']['name'],
+            'predictions_winner_comment': prediction['predictions']['winner']['comment'],
+            'predictions_win_or_draw': prediction['predictions']['win_or_draw'],
+            'predictions_under_over': prediction['predictions']['under_over'],
+            'predictions_goals_home': prediction['predictions']['goals']['home'],
+            'predictions_goals_away': prediction['predictions']['goals']['away'],
+            'predictions_advice': prediction['predictions']['advice'],
+            'predictions_percent_home': prediction['predictions']['percent']['home'],
+            'predictions_percent_draw': prediction['predictions']['percent']['draw'],
+            'predictions_percent_away': prediction['predictions']['percent']['away'],
+            'league_id': prediction['league']['id'],
+            'league_name': prediction['league']['name'],
+            'league_country': prediction['league']['country'],
+            'league_logo': prediction['league']['logo'],
+            'league_flag': prediction['league']['flag'],
+            'league_season': prediction['league']['season'],
+            'teams_home_id': prediction['teams']['home']['id'],
+            'teams_home_name': prediction['teams']['home']['name'],
+            'teams_home_logo': prediction['teams']['home']['logo'],
+            'teams_away_id': prediction['teams']['away']['id'],
+            'teams_away_name': prediction['teams']['away']['name'],
+            'teams_away_logo': prediction['teams']['away']['logo']
+         }
 
-    prediction_id = db.execute(insert(models.Predictions).values(**pred_row).returning(models.Predictions.id)).scalar_one()
-    print(f"Inserted a row with prediction_id: {prediction_id} and fixture_id: {fixture_id} into Predictions table.")
+        db.query(models.Predictions).filter(models.Predictions.fixture_id == fixture_id).update(pred_row)
+
+        print(f"Updated prediction with fixture_id: {fixture_id}")
+    else:
+        print(f"No prediction found for fixture_id {fixture_id}. Creating new record...")
+        
+        home_team_name = prediction['teams']['home']['name']
+        away_team_name = prediction['teams']['away']['name']
+
+        pred_row = {
+            'fixture_id': fixture_id,
+            'predictions_winner_id': prediction['predictions']['winner']['id'],
+            'predictions_winner_name': prediction['predictions']['winner']['name'],
+            'predictions_winner_comment': prediction['predictions']['winner']['comment'],
+            'predictions_win_or_draw': prediction['predictions']['win_or_draw'],
+            'predictions_under_over': prediction['predictions']['under_over'],
+            'predictions_goals_home': prediction['predictions']['goals']['home'],
+            'predictions_goals_away': prediction['predictions']['goals']['away'],
+            'predictions_advice': prediction['predictions']['advice'],
+            'predictions_percent_home': prediction['predictions']['percent']['home'],
+            'predictions_percent_draw': prediction['predictions']['percent']['draw'],
+            'predictions_percent_away': prediction['predictions']['percent']['away'],
+            'league_id': prediction['league']['id'],
+            'league_name': prediction['league']['name'],
+            'league_country': prediction['league']['country'],
+            'league_logo': prediction['league']['logo'],
+            'league_flag': prediction['league']['flag'],
+            'league_season': prediction['league']['season'],
+            'teams_home_id': prediction['teams']['home']['id'],
+            'teams_home_name': prediction['teams']['home']['name'],
+            'teams_home_logo': prediction['teams']['home']['logo'],
+            'teams_away_id': prediction['teams']['away']['id'],
+            'teams_away_name': prediction['teams']['away']['name'],
+            'teams_away_logo': prediction['teams']['away']['logo']
+        }
+
+        prediction_id = db.execute(insert(models.Predictions).values(**pred_row).returning(models.Predictions.id)).scalar_one()
+        print(f"Inserted a row with prediction_id: {prediction_id} and fixture_id: {fixture_id} into Predictions table.")        
+
+    # Now, for the comparisons, we need to update or create new rows
+    # First, get the prediction_id whether it was updated or created
+    prediction_id = db.query(models.Predictions.id).filter(models.Predictions.fixture_id == fixture_id).scalar()
 
     comp_rows = []
     for comparison_type, comparison_values in prediction['comparison'].items():
@@ -107,11 +148,15 @@ async def create_prediction(fixture_id: int, db: Session = Depends(get_db)):
                 }
                 comp_rows.append(comp_row)
 
+    # Delete existing comparisons for this prediction
+    db.query(models.PredictionComparisons).filter(models.PredictionComparisons.prediction_id == prediction_id).delete()
+
+    # Insert new comparisons
     db.execute(insert(models.PredictionComparisons).values(comp_rows))
 
     db.commit()
-    print(f"Inserted {len(comp_rows)} rows with prediction_id: {prediction_id} and fixture_id: {fixture_id} into PredictionComparisons table.")
-    return {"message": "Prediction created successfully!"}
+    print(f"Inserted/Updated {len(comp_rows)} rows with prediction_id: {prediction_id} and fixture_id: {fixture_id} into PredictionComparisons table.")
+    return {"message": "Prediction and comparisons created/updated successfully!"}
 
 @router.get("/uploadpredictions/{league_id}")
 async def update_all_predictions(league_id: int, db: Session = Depends(get_db)):
@@ -132,15 +177,21 @@ async def update_all_predictions(league_id: int, db: Session = Depends(get_db)):
     fixtures_data = fixtures_response.json()
     fixtures = fixtures_data.get('response', [])
 
+    print(f"Retrieved {len(fixtures)} fixtures for league_id: {league_id}")
+
     # Then for each fixture_id, we call the create_prediction function
-    for fixture in fixtures:
+    for i, fixture in enumerate(fixtures, 1):
         try:
             fixture_id = int(fixture['fixture']['id'])
-            print(f"Processing fixture_id: {fixture_id}")
+            print(f"Processing fixture_id: {fixture_id} ({i} of {len(fixtures)})")
             await asyncio.sleep(2)  # Add this line to introduce a 1-second delay
             await create_prediction(fixture_id, db)
+            
+            print(f"Finished processing fixture_id: {fixture_id}")
         except ValueError as e:
            print(f"Could not convert fixture_id to an integer: {fixture['fixture']['id']}")
             #traceback.print_exc()
 
+    print("Finished updating all predictions.")
+    
     return {"message": "Updated all predictions."}
